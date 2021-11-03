9a25c1cf85d0a84be78e7976b0887fcb585916f0
RaulWong12 <daniel.wong@hiumanlab.com>
2021-11-03 08:51:08 -0600

    api-mvp

---
+++ dist/routes/dispositivo.js
@@ -0,0 +1,281 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _express = require("express");
+
+var _database = require("../database");
+
+var _mongodb = require("mongodb");
+
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+
+var router = (0, _express.Router)(); // DB Connection
+
+router.get('/', /*#__PURE__*/function () {
+  var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(req, res) {
+    var db, result;
+    return regeneratorRuntime.wrap(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            _context.next = 2;
+            return (0, _database.connect)();
+
+          case 2:
+            db = _context.sent;
+            _context.next = 5;
+            return db.collection('mvp').find({}).toArray();
+
+          case 5:
+            result = _context.sent;
+            res.json(result);
+
+          case 7:
+          case "end":
+            return _context.stop();
+        }
+      }
+    }, _callee);
+  }));
+
+  return function (_x, _x2) {
+    return _ref.apply(this, arguments);
+  };
+}());
+router.post('/', /*#__PURE__*/function () {
+  var _ref2 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee2(req, res) {
+    var db, mvp, result;
+    return regeneratorRuntime.wrap(function _callee2$(_context2) {
+      while (1) {
+        switch (_context2.prev = _context2.next) {
+          case 0:
+            _context2.next = 2;
+            return (0, _database.connect)();
+
+          case 2:
+            db = _context2.sent;
+            mvp = {
+              modelo: req.body.modelo,
+              index: req.body.index,
+              photo: req.body.photo
+            };
+
+            if (!(mvp.modelo == null || mvp.index == null || mvp.photo == "" || mvp.modelo == "" || mvp.index == "" || mvp.photo == "")) {
+              _context2.next = 6;
+              break;
+            }
+
+            return _context2.abrupt("return", res.status(400).json({
+              msg: "bad request",
+              status: "400"
+            }));
+
+          case 6:
+            _context2.next = 8;
+            return db.collection('mvp').insertOne(mvp);
+
+          case 8:
+            result = _context2.sent;
+            res.json({
+              status: "200"
+            });
+
+          case 10:
+          case "end":
+            return _context2.stop();
+        }
+      }
+    }, _callee2);
+  }));
+
+  return function (_x3, _x4) {
+    return _ref2.apply(this, arguments);
+  };
+}());
+router.get('/modelo/:modelo', /*#__PURE__*/function () {
+  var _ref3 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee3(req, res) {
+    var modelo, db, result;
+    return regeneratorRuntime.wrap(function _callee3$(_context3) {
+      while (1) {
+        switch (_context3.prev = _context3.next) {
+          case 0:
+            modelo = req.params.modelo;
+            _context3.next = 3;
+            return (0, _database.connect)();
+
+          case 3:
+            db = _context3.sent;
+            _context3.next = 6;
+            return db.collection('mvp').find({
+              modelo: modelo
+            }).toArray();
+
+          case 6:
+            result = _context3.sent;
+            res.json(result);
+
+          case 8:
+          case "end":
+            return _context3.stop();
+        }
+      }
+    }, _callee3);
+  }));
+
+  return function (_x5, _x6) {
+    return _ref3.apply(this, arguments);
+  };
+}());
+router.get('/modelos/', /*#__PURE__*/function () {
+  var _ref4 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee4(req, res) {
+    var db, result;
+    return regeneratorRuntime.wrap(function _callee4$(_context4) {
+      while (1) {
+        switch (_context4.prev = _context4.next) {
+          case 0:
+            _context4.next = 2;
+            return (0, _database.connect)();
+
+          case 2:
+            db = _context4.sent;
+            _context4.next = 5;
+            return db.collection('mvp').distinct("modelo");
+
+          case 5:
+            result = _context4.sent;
+            res.json(result);
+
+          case 7:
+          case "end":
+            return _context4.stop();
+        }
+      }
+    }, _callee4);
+  }));
+
+  return function (_x7, _x8) {
+    return _ref4.apply(this, arguments);
+  };
+}());
+router.get('/:id', /*#__PURE__*/function () {
+  var _ref5 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee5(req, res) {
+    var id, db, result;
+    return regeneratorRuntime.wrap(function _callee5$(_context5) {
+      while (1) {
+        switch (_context5.prev = _context5.next) {
+          case 0:
+            id = req.params.id;
+            _context5.next = 3;
+            return (0, _database.connect)();
+
+          case 3:
+            db = _context5.sent;
+            _context5.next = 6;
+            return db.collection('mvp').findOne({
+              _id: (0, _mongodb.ObjectID)(id)
+            });
+
+          case 6:
+            result = _context5.sent;
+            res.json(result);
+
+          case 8:
+          case "end":
+            return _context5.stop();
+        }
+      }
+    }, _callee5);
+  }));
+
+  return function (_x9, _x10) {
+    return _ref5.apply(this, arguments);
+  };
+}());
+router["delete"]('/:id', /*#__PURE__*/function () {
+  var _ref6 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee6(req, res) {
+    var id, db, result;
+    return regeneratorRuntime.wrap(function _callee6$(_context6) {
+      while (1) {
+        switch (_context6.prev = _context6.next) {
+          case 0:
+            id = req.params.id;
+            _context6.next = 3;
+            return (0, _database.connect)();
+
+          case 3:
+            db = _context6.sent;
+            _context6.next = 6;
+            return db.collection('mvp').remove({
+              _id: (0, _mongodb.ObjectID)(id)
+            });
+
+          case 6:
+            result = _context6.sent;
+            res.json({
+              message: "dispositivo ".concat(id, " Deleted"),
+              result: result
+            });
+
+          case 8:
+          case "end":
+            return _context6.stop();
+        }
+      }
+    }, _callee6);
+  }));
+
+  return function (_x11, _x12) {
+    return _ref6.apply(this, arguments);
+  };
+}());
+router.put('/:id', /*#__PURE__*/function () {
+  var _ref7 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee7(req, res) {
+    var id, newMvp, db, result;
+    return regeneratorRuntime.wrap(function _callee7$(_context7) {
+      while (1) {
+        switch (_context7.prev = _context7.next) {
+          case 0:
+            id = req.params.id;
+            newMvp = {
+              modelo: req.body.modelo,
+              index: req.body.index,
+              photo: req.body.photo
+            };
+            _context7.next = 4;
+            return (0, _database.connect)();
+
+          case 4:
+            db = _context7.sent;
+            _context7.next = 7;
+            return db.collection('mvp').updateOne({
+              _id: (0, _mongodb.ObjectID)(id)
+            }, {
+              $set: newMvp
+            });
+
+          case 7:
+            result = _context7.sent;
+            res.json({
+              message: "dispositivo ".concat(id, " Updated")
+            });
+
+          case 9:
+          case "end":
+            return _context7.stop();
+        }
+      }
+    }, _callee7);
+  }));
+
+  return function (_x13, _x14) {
+    return _ref7.apply(this, arguments);
+  };
+}());
+var _default = router;
+exports["default"] = _default;
